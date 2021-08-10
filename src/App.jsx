b913bc19b95437c8e0ee1a946b16ff1944d54d1e
naveen0-0 <Naveen>
2021-08-10 11:52:02 +0530

    Added a route to check netlify and heroku route clashes

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HomePage from './components/HomePage';
+import About from './components/About';
 
-function App() {
-
-  const [persons,setPersons] = useState([]);
-  const [loading,setLoading] = useState(true);
-  const [msg,setMsg] = useState(null);
-  const [name,setName] = useState("")
-
-  const fetchPersons = async () => {
-    try {
-      const { data } = await axios.get('https://samplereact12905.herokuapp.com/allpersons');
-      if(data.statusload){
-        setPersons(data.persons);
-        setLoading(false);
-      }else{
-        setMsg(data.msg);
-        setLoading(false);
-      }
-    } catch (error) {
-      setMsg("Error while fetching data");
-      setLoading(false);
-    }
-  }
-
-  const formSubmit = async e => {
-    e.preventDefault();
-    try {
-      let {data} = await axios.post('https://samplereact12905.herokuapp.com/create',{name:name});
-      if(data.statusload){
-        setPersons(()=>{
-          return [...persons,{name:name}]
-        })
-      }
-    } catch (error) {
-      setMsg("Unable to post");
-    }
-  }
-
-  useEffect(()=>{
-    fetchPersons();
-  },[])
-
-  if(loading){
-    return (
-      <div>
-        Fetching Data
-      </div>
-    )
-  }else{
-    return (
-      <div>
-        <div>
-          <form onSubmit={formSubmit}>
-            <input type="text" onChange={(e) => setName(e.target.value)} value={name}/>
-            <input type="submit" value="Send"/>
-          </form>
-        </div>
-        <div>
-          {persons.map((person,index)=>{
-            return (
-              <div key={index}>
-                {person.name}
-              </div>
-            )
-          })}
-        </div>
-
-        <div>
-          {msg}
-        </div>
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact><HomePage/></Route>
+        <Route path="/about"><About/></Route>
+      </Switch>
+    </Router>
+  )
 }
-
-export default App
